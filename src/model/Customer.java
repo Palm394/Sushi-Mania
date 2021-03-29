79b52e1ee0a2f39f9cd2389f94b844f394e6985a
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2021-03-30 00:28:14 +0700

    Create Customer

---
+++ src/model/Customer.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Customer {
+	
+	//order integer is sorting customer to check food to detect.
+	private int order;
+	
+	public Customer(int order) {
+		this.order = order;
+	}
+}
