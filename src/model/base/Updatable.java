8e34df88d026fc82dc78464bcfcc7a2384f2311f
Nichapat Kerkkittikul <63848208+Palm394@users.noreply.github.com>
2021-04-06 19:22:57 +0700

    Update Updatable.java

--- src/model/base/Updatable.java
+++ src/model/base/Updatable.java
@@ -5,7 +5,5 @@ import javafx.scene.canvas.GraphicsContext;
 public interface Updatable {
 	
     public void update();
-    public void draw(GraphicsContext gc);
-    public boolean isDestroyed();
     
 }
