466850bee883283ffe7782b24082e2fcea7c5021
Chanathip Sombuttong <oommankung@gmail.com>
2021-04-06 18:25:40 +0700

    create some button and just a little structure

--- src/gui/gamePane.java
+++ src/gui/gamePane.java
@@ -1,13 +1,19 @@
 package gui;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class gamePane extends Canvas  {
 	
-	private VBox root;
+	private VBox root,head;
+	private HBox bottom;
 	private Scene gameScene;
 	private Stage gameStage;
 	
@@ -21,17 +27,22 @@ public class gamePane extends Canvas  {
 		gameScene = new Scene(root,WIDTH_SIZE,HEIGHT_SIZE);
 
 		//--- Status Bar ---
-		
-		
 		//--- Customer Zone ---
-		
-		
-		//--- Table Zone ---
-		
-		
+		//--- Table Zone --
+		head = new VBox();
+		head.setPrefSize(1000, 300);
 		//--- Chef Zone ---
-		
-		
+		Parent bottom;
+		try {
+			bottom = FXMLLoader.load(getClass().getResource("chefZone.fxml"));
+		// add components
+		root.getChildren().add(head);
+		root.getChildren().add(bottom);		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		//--- End ---
 		
 		gameStage = new Stage();
