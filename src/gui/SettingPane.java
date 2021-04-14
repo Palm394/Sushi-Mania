ff00aca146821ef697cae34173339ef4d3f0d732
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2021-04-14 11:11:38 +0700

    VolLabel can follow vol

--- src/gui/SettingPane.java
+++ src/gui/SettingPane.java
@@ -1,14 +1,19 @@
 package gui;
 
+import java.text.DecimalFormat;
+
 import application.Utility;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.text.Font;
 import model.AlertPane;
 
 public class SettingPane extends AlertPane {
 
+	private Label Sound_Label;
+	
 	public SettingPane(String title, double W, double H) {
 		super(title, W, H);
 		setSliderVol();
@@ -16,7 +21,8 @@ public class SettingPane extends AlertPane {
 	
 	@Override
 	protected void addCenter() {
-		Label Sound_Label = new Label("Sound Volume");
+		Sound_Label = new Label("Sound Volume : " + 50);
+		Sound_Label.setFont(new Font(20));
 		center.getChildren().add(Sound_Label);
 		
 		center.getChildren().add(setSliderVol());
@@ -32,6 +38,7 @@ public class SettingPane extends AlertPane {
 			@Override
 			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
 				Utility.setAllVolume((double) newValue);
+				Sound_Label.setText("Sound Volume : " + ((int)(Utility.volume *100)));
 			}
 
 		});
