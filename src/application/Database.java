17c2876a4575048f4f839d6b60ba2952e52eea3e
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2021-04-19 21:30:11 +0700

    add hasIngredients

--- src/application/Database.java
+++ src/application/Database.java
@@ -3,13 +3,18 @@ package application;
 import java.util.ArrayList;
 
 import model.FoodList;
+import model.Ingredient;
 
 public class Database {
 	private static ArrayList<FoodList> hasMenu = new ArrayList<FoodList>();
+	private static ArrayList<Ingredient> hasIngredient = new ArrayList<Ingredient>();
+	
 	
 	public static void initialize() {
 		//Mock for level 1
 		hasMenu.add(new FoodList(28700092));
+		
+		hasIngredient.add(new Ingredient(0));
 	}
 	
 	public static void newLV(int level) {
@@ -21,5 +26,13 @@ public class Database {
 				hasMenu.add(new FoodList(14545740));
 		}
 			
+	}
+
+	public static ArrayList<FoodList> getHasMenu() {
+		return hasMenu;
+	}
+
+	public static ArrayList<Ingredient> getHasIngredient() {
+		return hasIngredient;
 	}
 }
